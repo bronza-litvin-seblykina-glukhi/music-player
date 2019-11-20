77e6767a45b2fc715acc451154a29234fe724d88
NikitaGlukhi <nikita.glukhi@valor-software.com>
2019-11-20 17:17:22 +0200

    feat(core): added environment variables

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,12 @@
+require('dotenv/config');
+
 module.exports = {
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '1',
-  database: 'music_player',
+  type: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: ['apps/server/src/modules/**/**/*.entity.ts'],
   migrationsTableName: 'migration_table',
   migrations: ['apps/server/src/migration/**/*.ts'],
